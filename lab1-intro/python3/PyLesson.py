db49ed7963484bdee15b91e3ebe7c1f6506d791c
StackCodr <xxpeaceaxx@gmail.com>
2026-05-03 12:08:09 +0000

    Python Lesson (part 1 on Texts and Math)

---
+++ lab1-intro/python3/PyLesson.py
@@ -0,0 +1,37 @@
+
+# ---- PYTHON LESSON -----
+
+# Comments: Hash tags for single-lined comment
+# Comments: """ for multi-lined comment?
+
+# ---- (1) PYTHON INTERACTIVE MODE (Python in Terminal) ----
+
+# Prompts (MATH and DIVISION OPERATORS):
+
+# Math
+# "number ** power_value" raises number to power of power_value e.g. number ** 2, is number squared.
+# "2+2" Terminal can calculate expressions and with other operators, + - / etc
+
+# Division
+# "number / number" is normal division, returns a floating number.
+# "number // number" is a floor division, that returns value without fractional part. (Becomes integer)
+# "number % number" is a modulo operator, that returns the remaining part of floating number. (Becomes integer)
+
+# Prompts (TEXTS):
+# Usage of println: Using variable without print() includes special characters in output, otherwise they are interpreted
+
+# Escapes
+# Newline: Use "\n" special character to create new line (important to not add space between this and next line)
+# Tab: Use "\t" special character to create tab
+# Quotes: Use "\" special character to escape quotes or have alternate use of quotes
+# Backslash: Use "r" prefix for quote in print() to make "\"" not interpreted as a special character. For example, print(r'C:\this\name')
+
+# Texts
+# Multi-lined texts: Triple-quoted strings (''' or """) with text enclosed them create a multi-line text (also known as string literals), it includes newline each time ENTER is pressed; a "\" at the end of a line prevents that newline.
+# String concatenation (part 1): Two string literals written adjacently are automatically concatenated, more explicitly using "+" in-between.
+# Continue in doc from "This only works with two literals though, not with variables or expressions:"
+
+# SPECIAL PARTS OF PROMPT:
+# "_" : this operator is where the last result of printed expression is stored. If you manual attach value to it, it overrides the magical feature.
+# "round(x,y)": built-in function that allows to round x to y decimals. For example, round(2.234, 2) returns 2.23.
+
