77a9742b35fe99baf7aad7a7d83307c2c5f13ba1
StackCodr <xxpeaceaxx@gmail.com>
2026-05-05 05:02:36 +0000

    Haskell Lesson: Took notes

--- lab2-functionalPrimer/haskell/intro1.hs
+++ lab2-functionalPrimer/haskell/intro1.hs
@@ -1,29 +1,131 @@
 module Intro1 where
 
 -- define named constants:
+
+r1 :: Integer
 r1 = 1
-r2 = b^2 + 1/b where b = 2
+-- Yellow warning, Resolved: Type signature was missing
+{-
+WARNING EXPLANATION
+Haskell applies Monomorphism restrictions (force it to have one concrete type) if its a top-level value with no arguments (not a function)
+-}
+
+r2 :: Double
+r2 = b**2 + 1/b where b = 2
+-- Yellow warning, Resolved: Type inconsistency across expression occured.
+{- 
+WARNING EXPLANATION
+(^) was used, exponent must be an integral type (i.e. Int, Integer)
+(a / b) was used, both a and b must be a fractional type (i.e. Float or Double)
+The expression was unified type-wise (to Fractional) by replacing (^) with (**)
+(a ** b) expects both a and b to be of a floating type (e.g. Float, Double).
+-}
 
 -- define a function:
+diff :: Num a => a -> a -> a
 diff a b = abs (a - b)
+-- Yellow warning, Resolved: Type signature was missing
+-- abs() is a built-in Haskell function that returns absolute value of number (result always non-negative)
+-- diff function takes in two arguments.
+{-
+TYPE SIGNATURE EXPLANATION
+• A type signature describes what kinds of values a function takes and what it returns.  
+• A type variable (like a) is a placeholder that can represent any type, making functions reusable for many types.  
+• A type constraint (like Num a =>) restricts those variables to a group of types that share certain operations.  
+• The => symbol separates constraints from the main part of the type, e.g. Num a => a -> a -> a.  
+• These general (polymorphic) type signatures are just flexible versions of fixed ones like Integer -> Integer -> Integer.
+-}
 
+r3, r4 :: Integer
 r3 = diff (diff a a) a  where a = 1
 r4 = diff (diff a b) b  where a = 1; b = 1
+-- Yellow warning, Resolved: Type signature was missing
+{-
+CODE FUNCTIONALITY r3
+1. where a = 1 defines local variable, a = 1
+2. (diff a a) is an inner call, result is substituted into outer call diff (x) a, where x is result.
+
+CODE FUNCTIONALITY r4
+1. where a = 1; b = 1 defines local variables, a = 1 and b = 1
+2. similar to r3
+
+"where" keyword introduces local variables:
+    - each one is separated by semi-colons (inline way).
+    - each one is placed on new line, with indentation
+-}
 
 -- lists by enumeration
+r5, r6 :: [Integer]
 r5 = [1,3,4,1]
 r6 = [1..5]
+-- Yellow warning, Resolved: Type signature was missing
 
+inc :: Num a => a -> a
 inc n = n + 1
+-- Yellow warning, Resolved: Type signature was missing
 
-r7 = map inc [1..3]
+r7, r8 :: [Integer]
+r9 :: [Double]
+r7 = map inc [1..3] 
 r8 = map (diff 2) [1..3]
 r9 = map sqrt [1..3]
+-- Yellow warning, Resolved: Type signature was missing
 
+{-
+CODE FUNCTIONALITY r7
+"[1..3]": Create list from 1 to 3.
+map:
+    - Haskell built-in function, takes a list and function (in its type signature, function is denoted by (a -> b)).
+    - Purpose, applies function to every element of a list, producing a new list of results
+    - map only works with lists and produces lists
+
+In r8, partial application is used—by giving diff only its first argument (2), it creates a new one‑argument function that’s later applied to each list element by map.
+
+-}
+
+r10, r11 :: [(Integer, Double)]
 r10 = zip [1..3] (map sqrt [1..3])
 r11 = zip [1..3] (map sqrt [1..2])
+-- Yellow warning, Resolved: Type signature was missing
+{-
+TYPE SIGNATURE EXPLANATION
+r10, r11 :: [(Integer, Double)]
+- each variable (r10 and r11) holds a list of tuples (i.e. x, y), the tuple (pair) being Integer and Double
+- Summary: variables hold list of Integer, Double pairs
 
+CODE FUNCTIONALITY r10
+- computation: usually evaluated from right to left
+    - (map sqrt [1..3]): map applies sqrt function to each element in list and generates a new list
+    - zip [1..3] (x), where x is result of map (list): zip combines two lists into one list of pairs (tuples).
+
+zip:
+    - Haskell built-in function that combines two lists into one, producing list with tuples (denoted in type signature by [a] -> [b] -> [(a,b)])
+    - If one list is shorter, the zip stops.
+    - Example:`zip [1,2,3] [10,20]`, will output [(1,10),(2,20)]
+
+-}
+
+r12 :: IO()
 r12 = print [1..3]
+-- Yellow warning p1, Resolved: Type signature was missing
+{-
+TYPE SIGNATURE EXPLANATION
+r12 :: IO()
+- r12 is an IO action
+- IO() type means r12 is an action that interacts with the outside world (e.g. print) and, when it’s done, returns () nothing useful 
+
+CODE FUNCTIONALITY
+- list is created in range 1 to 3
+- print outputs list to the screen
+
+print:
+    - Haskell built-in function that returns an action, something that runtime performs later when program runs.
+-}
 
 main :: IO ()
 main = pure ()
+-- Yellow warning p1, Resolved: Type signature was missing
+{-
+CODE FUNCTIONALITY
+pure: Haskell built-in function that is a do-nothing IO action
+-}
--- lab2-functionalPrimer/haskell/intro2.hs
+++ lab2-functionalPrimer/haskell/intro2.hs
@@ -3,36 +3,42 @@ module Intro2 where
 -- String algebra:
 name = "Alice"
 -- "if" has a special syntax but otherwise a typed version of Lisp's "if":
-name2 = if name /= "" then name else "no name"
+name2 = if name /= "" then name else "no name" -- "NOTE: /= means not equal"
 
 pname3 = print "Bob"
 
 myprogram = print (1 + m)  -- compiler error: m undefined
 m = 1                      -- unless this line is also present
 
--- sequencing several imperative programs:
-prg1 = do
+-- sequencing several imperative programs (they are programs made up of statements that run one after another):
+prg1 = do -- NOTE: do introduces a block of sequential IO actions
   print "hello " 
   print name  -- level of indentation is important
 
 -- the same, but using algebra of imperative programs:
 prg1' = sequence_ [print "hello ", print name]
+-- NOTE: sequence_ , built-in function that takes list of actions and runs them in order (ignores their results and returns combined IO() action)
 
 -- one imperative program passing value to another:
 prg2 = do
   line <- getLine
   putStrLn ("you typed: " ++ line)
+-- NOTE: getLine, IO action that waits for user to input something and then returns it as a string. 
+  -- Use of arrow can bind result to a name (In Haskell, the arrow means "run getLine and name its result x", its not about storing a value in a variable)
+-- NOTE: putStrLn , IO action that takes a string and prints it to a new line
+-- NOTE: ++ operator ,can be used to concatenate strings.
 
-main =
-  do
+main =  do
   putStrLn name -- like print, but only for strings
   -- putStrLn pname3 -- Couldn't match type ...; Expected type: String; Actual type: IO ()
   pname3 -- in Lisp: eval pname3
-  myprogram; prg1; prg2 -- sequencing, like 3 lines
-
+  myprogram; prg1; prg2;ello -- sequencing, like 3 lines
+-- NOTE:  putStrLn : Mentioned in prg2. But additionally, it only works with strings compared to print.
+-- NOTE:  multiple statements can be in one line, separated by semicolons. They will execute in order, from left to right.
 
 -- generic string conversion, using a Java-friendly name:
 toString value = show value
+-- NOTE: show, a built-in function that converts any printable value into a String
 
 -- string concatenation:
 greet1 = "hello " ++ name ++ (toString 123)
@@ -40,3 +46,9 @@ greet1 = "hello " ++ name ++ (toString 123)
 greet2 = (++) "hello " name
 -- concatenating more than 2 strings:
 greet3 = concat ["hello ", name, toString 123]
+
+ello = do
+  print greet1
+-- NOTE: This is an ready-to-use imperative program.
+-- NOTE: main is the only imperative program that runs, so everything else has to be into here to execute.
+-- NOTE: Difference between show and print, show does not print anything and just returns a string but print displays value to screen and its an IO action
