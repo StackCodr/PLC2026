3f4b113b11faabd9e35b69c87229107255526183
StackCodr <xxpeaceaxx@gmail.com>
2026-05-05 15:21:59 +0000

    Ada, Java and Lisp Lesson (W3) : Did tasks

--- lab3-num/java/Overflow.java
+++ lab3-num/java/Overflow.java
@@ -86,7 +86,20 @@ public class Overflow
         for ( int j = 1; j <= m; j++ )
         {
             ns = ns * n;
-//             assert ?? : "Floating-point overflow";
+
+            // Model answer:
+            assert ns * 2 != ns :"Floating-point overflow";
+
+            // Solution 1: Use float method
+            //assert !Float.isInfinite(ns) : "Floating-point overflow";
+            
+            // Solution 2: Testing ns (float) against max float representation
+            // assert ns <= Float.MAX_VALUE : "Floating-point overflow";
+            
+            // Solution 3: Testing ns (float) against larger data type (double)
+            // assert Math.pow(n, m) > ns: "Floating-point overflow";
+            // ns will reach infinity sooner than double (double maxes out much later)
+            
             resultList.add(ns);
         }
         return resultList;
@@ -112,7 +125,7 @@ public class Overflow
         for ( int j = 0; j <= m; j++ )
         {
             ns_inv = ns_inv / n; // update from 1/n^(i-1) to 1/n^i
-//             assert ?? : "Floating point underflow";
+            assert ns_inv != 0 : "Floating point underflow";
             geom_sum = ns_inv + geom_sum;
             resultList.add(geom_sum);
         }
